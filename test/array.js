06d49636e7d6483a4c59ceae855c484b9305442a
Feifei <feli@sonicwall.com>
2022-01-09 16:59:18 +0800

    fix: others.md

--- test/array.js
+++ test/array.js
@@ -1,8 +1,10 @@
-const map = new Map([[{}, 45]]);
-map.set('feli', '30');
-// console.log([...map]);
-
-const id = 5;
-if(id === 1 || null) {
-    console.log(333);
+const demo = async ()=>{
+    let result = await setTimeout(()=>{
+      console.log('我延迟了一秒');
+    }, 1000)
+    console.log('我由于上面的程序还没执行完，先不执行“等待一会”');
+    return result
 }
+demo().then(result=>{
+  console.log('输出',result);
+})
